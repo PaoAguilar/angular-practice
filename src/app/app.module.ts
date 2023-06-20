7100975fd9c85ef36c9a3b47ac7a6367bbda1121
Paola Aguilar <pao.aquev29@gmail.com>
2023-06-20 14:41:45 -0600

    add json server and new service to delete task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgOptimizedImage } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
-import { TasksComponent } from './components/tasks/tasks.component';
+import { HeaderComponent } from './components/header/header.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
